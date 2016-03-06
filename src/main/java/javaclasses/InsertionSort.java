192afdefe54fac59d156f767bfb233a3751e5af8
adamhurwitz <adamscotthurwitz@gmail.com>
2016-03-06 00:08:54 -0800

    feat: implement insertion sort

--- src/main/java/javaclasses/InsertionSort.java
+++ src/main/java/javaclasses/InsertionSort.java
@@ -6,7 +6,42 @@ package javaclasses;
 public class InsertionSort {
     static private final String LOG_TAG = InsertionSort.class.getSimpleName();
 
-    static int[] array = {3, 5, 7, 11, 13, 2, 9, 6};
+    static int[] array = {22, 11, 99, 88, 9, 7, 42};
+
+    public static void main(String[] arg) {
+        System.out.println("Answer is: " + insertionSort(array));
+    }
+
+    public static String insertionSort(int[] array){
+        int rightIndex = array.length - 2;
+        int value = array[0];
+        System.out.println("rightIndex: " + rightIndex + " value: " + value);
+        return insertValue(array, rightIndex, value);
+    }
+
+    public static String insertValue(int[] array, int rightIndex, int value) {
+        String solution = "";
+
+        for (int compareIndex = array.length - 1; compareIndex >= 0; compareIndex--) {
+            if (compareIndex <= rightIndex
+                    && array[compareIndex] > value
+                    ) {
+                System.out.println(compareIndex);
+                int temp = array[compareIndex + 1];
+                array[compareIndex + 1] = array[compareIndex];
+                array[compareIndex] = temp;
+            }
+        }
+
+        System.out.println("Array Length " + array.length);
+
+        for (int i = 0; i < array.length; i++) {
+            solution += " " + array[i];
+        }
+        return solution;
+    }
+
+    /*static int[] array = {3, 5, 7, 11, 13, 2, 9, 6};
 
     public static void main(String[] arg) {
         System.out.println("Answer is: " + insertValue(array, 4, 2));
@@ -32,5 +67,5 @@ public class InsertionSort {
             solution += " " + array[i];
         }
         return solution;
-    }
+    }*/
 }
