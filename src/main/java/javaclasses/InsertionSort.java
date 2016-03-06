e7aa800b431524c56437be5797c5264855f9f663
adamhurwitz <adamscotthurwitz@gmail.com>
2016-03-06 02:19:33 -0800

    refactor: insertion sort

--- src/main/java/javaclasses/InsertionSort.java
+++ src/main/java/javaclasses/InsertionSort.java
@@ -6,78 +6,79 @@ package javaclasses;
 public class InsertionSort {
     static private final String LOG_TAG = InsertionSort.class.getSimpleName();
 
-    static int[] array = {22, 11, 85, 88, 9, 7, 42};
+    static int[] A = {22, 11, 85, 88, 9, 7, 42};
 
     public static void main(String[] arg) {
-        System.out.println("Answer is: " + insertionSort(array));
+        System.out.println("Answer is: " + insertionSort(A
+        ));
     }
 
-    public static String insertionSort(int[] array) {
+    public static String insertionSort(int[] A) {
         String solution = "";
-        //TODO: Fix implementation
-        for (int i = 1; i < array.length; i++) {
-            int key = array[i];
+
+        for (int i = 1; i < A.length; i++) {
+            // key to keep track of which element currently is being evaluated
+            int key = A[i];
             int j = i - 1;
-            while (j >= 0 && array[j] > key) {
-                array[j + 1] = array[j];
+            while (j >= 0 && A[j] > key) {
+                A[j + 1] = A[j];
                 j--;
             }
-            array[j + 1] = key;
+            A[j + 1] = key;
 
-            //solution = insertValue(array, array.length - 2, i);
+            //solution = insertValue(A
+            // , A
+            // .length - 2, i);
         }
-        for (int i = 0; i < array.length; i++) {
-            solution += " " + array[i];
-        }
-        return solution;
-    }
-
-    public static String insertValue(int[] array, int rightIndex, int value) {
-        String solution = "";
-
-        for (int compareIndex = array.length; compareIndex >= 0; compareIndex--) {
-            //System.out.println(compareIndex);
-            if (compareIndex <= rightIndex && array[compareIndex] > value) {
-                int temp = array[compareIndex + 1];
-                array[compareIndex + 1] = array[compareIndex];
-                array[compareIndex] = temp;
-            }
-        }
-
-        System.out.println("Array Length " + array.length);
-
-        for (int i = 0; i < array.length; i++) {
-            solution += " " + array[i];
+        for (int i = 0; i < A.length; i++) {
+            solution += " " + A[i];
         }
         return solution;
     }
+}
 
 
-    /*static int[] array = {3, 5, 7, 11, 13, 2, 9, 6};
+// insertValue() exercise
+
+    /*static int[] A
+     = {3, 5, 7, 11, 13, 2, 9, 6};
 
     public static void main(String[] arg) {
-        System.out.println("Answer is: " + insertValue(array, 4, 2));
-        System.out.println("Answer is: " + insertValue(array, 5, 9));
-        System.out.println("Answer is: " + insertValue(array, 6, 6));
+        System.out.println("Answer is: " + insertValue(A
+        , 4, 2));
+        System.out.println("Answer is: " + insertValue(A
+        , 5, 9));
+        System.out.println("Answer is: " + insertValue(A
+        , 6, 6));
     }
 
-    public static String insertValue(int[] array, int rightIndex, int value) {
+    public static String insertValue(int[] A
+    , int rightIndex, int value) {
         String solution = "";
 
-        for (int compareIndex = array.length; compareIndex >= 0; compareIndex--) {
+        for (int compareIndex = A
+        .length; compareIndex >= 0; compareIndex--) {
             //System.out.println(compareIndex);
-            if (compareIndex <= rightIndex && array[compareIndex] > value) {
-                int temp = array[compareIndex + 1];
-                array[compareIndex + 1] = array[compareIndex];
-                array[compareIndex] = temp;
+            if (compareIndex <= rightIndex && A
+            [compareIndex] > value) {
+                int temp = A
+                [compareIndex + 1];
+                A
+                [compareIndex + 1] = A
+                [compareIndex];
+                A
+                [compareIndex] = temp;
             }
         }
 
-        System.out.println("Array Length " + array.length);
+        System.out.println("A
+         Length " + A
+         .length);
 
-        for (int i = 0; i < array.length; i++) {
-            solution += " " + array[i];
+        for (int i = 0; i < A
+        .length; i++) {
+            solution += " " + A
+            [i];
         }
         return solution;
     }*/
-}
